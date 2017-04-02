8566c5dbeb9541872bf63005ce74d70a7fa32c77
luke <lukeluluke@gmail.com>
2017-04-02 20:15:40 +1000

    1. Add create route for author 2. Add create, list, update and delete routes for article

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -2,70 +2,291 @@
 
 namespace AppBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\View\View;
 use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use AppBundle\Entity\Article;
 use AppBundle\Entity\Author;
 
-
 class ArticleController extends FOSRestController
 {
     const TYPE_RESPONSE_SUCCESS = 'success';
     const TYPE_RESPONSE_FAIL = 'fail';
 
+
     /**
-     * @Rest\Post("/author/create")
+     * @Rest\Get("/article/list")
      */
     public function listAction()
     {
-        return $this->render('AppBundle:Article:list.html.php', array(
-            // ...
-        ));
+        $result = [
+            'status' => $this::TYPE_RESPONSE_FAIL,
+            'error' => '',
+            'data' => []
+        ];
+
+        try{
+
+
+            $manager = $this->getDoctrine()->getManager();
+            $articles = $manager->getRepository('AppBundle:Article')->getArticleWithAuthorName();
+
+            $result['status'] = $this::TYPE_RESPONSE_SUCCESS;
+            $result['data'] = $articles;
+        }catch(Exception $e)
+        {
+            $result['status'] = $this::TYPE_RESPONSE_FAIL;
+            $result['error'] = 'Service not available';
+        }
+
+        return $result;
     }
 
     /**
      * @Rest\Get("/article/get/{id}")
      */
-    public function getAction()
+    public function getAction($id)
     {
-        return $this->render('AppBundle:Article:get.html.php', array(
-            // ...
-        ));
+        $result = [
+            'status' => $this::TYPE_RESPONSE_FAIL,
+            'error' => '',
+            'data' => []
+        ];
+
+        try{
+            $manager = $this->getDoctrine()->getManager();
+            $article = $manager->getRepository('AppBundle:Article')->getArticleWithAuthorName($id);
+
+            if($article)
+            {
+                $result['status'] = $this::TYPE_RESPONSE_SUCCESS;
+                $result['data']  = $article;
+            }else{
+                $result['error'] = 'No article found';
+            }
+        }catch(Exception $e)
+        {
+            $result['status'] = $this::TYPE_RESPONSE_FAIL;
+            $result['error'] = 'Service not available';
+        }
+
+        return $result;
+
     }
 
     /**
      * @Rest\Post("/article/create")
+     * @param Request $request
+     * @return array
      */
-    public function createAction()
+    public function createAction(Request $request)
     {
-        return $this->render('AppBundle:Article:create.html.php', array(
-            // ...
-        ));
+        $result = [
+            'status' => $this::TYPE_RESPONSE_FAIL,
+            'error' =>'',
+            'data' =>[]
+        ];
+
+        $model = new Article();
+
+        $author_id = $request->get('author_id');
+        $title = $request->get('title');
+        $url = $request->get('url');
+        $content = $request->get('content');
+
+        try{
+            if(!empty($author_id))
+            {
+                /**
+                 * Check if author_id exists, only save article when there is a author_id
+                 */
+                $author = $this->getDoctrine()
+                    ->getRepository('AppBundle:Author')
+                    ->find($author_id);
+
+                if($author)
+                {
+                    /**
+                     * Only save article when there is author
+                     */
+
+                    $model->setTitle($title);
+                    $model->setAuthorId($author_id);
+                    $model->setContent($content);
+                    $model->setUrl($url);
+                    $model->setCreatedAt(new \DateTime('now'));
+                    $model->setUpdatedAt(new \DateTime('now'));
+                    $model->setAuthor($author);
+
+                    $validator = $this->get('validator');
+                    $errors = $validator->validate($model);
+
+                    if(count($errors)>0)
+                    {
+                        /**
+                         * If entity does not pass validation
+                         */
+                        $result['error'] = $errors;
+                    }else{
+                        $em = $this->getDoctrine()->getManager();
+                        $em->persist($model);
+                        $em->flush();
+
+                        $data = [
+                            'title' => $model->getTitle(),
+                            'author' => $author->getName(),
+                            'summary' => $model->getContent(),
+                            'url' => $model->getUrl(),
+                            'createdAt' => $model->getCreatedAt()
+                        ];
+                        $result['status'] = $this::TYPE_RESPONSE_SUCCESS;
+                        $result['data'] = $data;
+                    }
+
+
+                }else{
+                    $result['error'] = 'No author found';
+                }
+            }else{
+                $result['error'] = 'No author id found';
+            }
+        }catch(Exception $e)
+        {
+            /**
+             * Todo need to log error
+             */
+            $result['status'] = $this::TYPE_RESPONSE_FAIL;
+            $result['error'] = 'Service not available';
+        }
+
+        return $result;
     }
 
     /**
-     *  @Rest\Put("/article/update/{id}")
+     * @Rest\Put("/article/update/{id}")
+     * @param $id
+     * @param Request $request
+     * @return array
      */
-    public function updateAction()
+    public function updateAction($id,Request $request)
     {
-        return $this->render('AppBundle:Article:update.html.php', array(
-            // ...
-        ));
+        $result = [
+            'status' => $this::TYPE_RESPONSE_FAIL,
+            'error' =>''
+        ];
+
+
+        try{
+            $author_id = $request->get('author_id');
+            $title = $request->get('title');
+            $content = $request->get('content');
+            $url = $request->get('url');
+
+
+            $model = $this->getDoctrine()->getRepository('AppBundle:Article')->find($id);
+
+            if($model)
+            {
+
+
+                if(!empty($author_id))
+                {
+                    $model->setAuthorId($author_id);
+                }
+
+                if(!empty($title))
+                {
+                    $model->setTitle($title);
+                }
+                if(!empty($content))
+                {
+                    $model->setContent($content);
+                }
+                if(!empty($url))
+                {
+                    $model->setUrl($url);
+                }
+
+
+                $validator = $this->get('validator');
+                $errors = $validator->validate($model);
+
+                $author = $this->getDoctrine()->getRepository('AppBundle:Author')->find($model->getAuthorId());
+                if(count($errors)>0)
+                {
+                    /**
+                     * If entity does not pass validation
+                     */
+                    $result['error'] = $errors;
+
+                }elseif(!$author)
+                {
+                    $result['error'] = 'Author not found';
+                }
+                else{
+                    $em = $this->getDoctrine()->getManager();
+                    $em->flush();
+
+                    $data = [
+                        'title' => $model->getTitle(),
+                        'summary' => $model->getContent(),
+                        'url' => $model->getUrl(),
+                        'createdAt' => $model->getCreatedAt()
+                    ];
+                    $result['status'] = $this::TYPE_RESPONSE_SUCCESS;
+                    $result['data'] = $data;
+                }
+
+            }else{
+                $result['error'] = 'No Article found';
+            }
+        }catch(Exception $e)
+        {
+            $result['status'] = $this::TYPE_RESPONSE_FAIL;
+            $result['error'] = 'Service not available';
+        }
+
+
+        return $result;
     }
 
     /**
      * @Rest\Delete("/article/delete/{id}")
      */
-    public function deleteAction()
+    public function deleteAction($id)
     {
-        return $this->render('AppBundle:Article:delete.html.php', array(
-            // ...
-        ));
+        $result = [
+            'status' => $this::TYPE_RESPONSE_FAIL,
+            'error' => '',
+            'data' => []
+        ];
+
+        try{
+            $em = $this->getDoctrine()->getManager();
+            $article = $em->getRepository('AppBundle:Article')->find($id);
+
+            if($article)
+            {
+                $em->remove($article);
+                $em->flush();
+                $result['status'] = $this::TYPE_RESPONSE_SUCCESS;
+
+            }else{
+                $result['error'] = 'No article found';
+            }
+        }catch(Exception $e)
+        {
+            $result['status'] = $this::TYPE_RESPONSE_FAIL;
+            $result['error'] = 'Service not available';
+        }
+
+        return $result;
+
     }
 
 }
--- src/AppBundle/Controller/AuthorController.php
+++ src/AppBundle/Controller/AuthorController.php
@@ -6,26 +6,76 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\View\View;
 use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use AppBundle\Entity\Author;
+
 
 class AuthorController extends FOSRestController
 {
 
     const TYPE_RESPONSE_SUCCESS = 'success';
     const TYPE_RESPONSE_FAIL = 'fail';
-
-
     /**
      * @Rest\Post("/author/create")
+     * @param Request $request
+     * @return array
      */
-    public function createAction()
+    public function createAction(Request $request)
     {
-        return $this->render('AppBundle:Author:create.html.php', array(
-            // ...
-        ));
+        $result = [
+            'status' => $this::TYPE_RESPONSE_FAIL,
+            'error' =>'',
+            'data' =>[]
+        ];
+        $model = new Author();
+        $name = $request->get('name');
+        try{
+            if(!empty($name))
+            {
+                /**
+                 * Check if author name exists, only save author when there is an author name
+                 */
+
+                $model->setName($name);
+
+
+                $validator = $this->get('validator');
+                $errors = $validator->validate($model);
+
+                if(count($errors)>0)
+                {
+                    /**
+                     * If entity does not pass validation
+                     */
+                    $result['error'] = $errors;
+                }else{
+                    $em = $this->getDoctrine()->getManager();
+                    $em->persist($model);
+                    $em->flush();
+
+                    $data = [
+                        'id' => $model->getId(),
+                        'name' => $model->getName()
+                    ];
+                    $result['status'] = $this::TYPE_RESPONSE_SUCCESS;
+                    $result['data'] = $data;
+                }
+            }else{
+                $result['error'] = 'Author name is required';
+
+            }
+        }catch(Exception $e)
+        {
+            /**
+             * Todo need to log error
+             */
+        }
+
+        return $result;
+
     }
 
 }
